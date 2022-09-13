6e9f83abccaad755ce5fa2c7ad7dae71cc55f0f8
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2022-09-12 23:37:01 -0500

    add codes

---
+++ termores 100/codigos/calibrationCode.ino
@@ -0,0 +1,31 @@
+#define RB 96.0
+#define VT 4.65
+#define LED 3
+#define termoRes A5
+
+int v = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+float vV(float v)
+{
+  return (v * 5.0 / 1023.0);
+}
+
+float rT(float v)
+{
+  float vp = vV(v);
+  return(RB * vp / (VT - vp));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  v = analogRead(termoRes);
+  Serial.print(v);
+  Serial.print("\t");
+  Serial.println(rT(v));
+}
